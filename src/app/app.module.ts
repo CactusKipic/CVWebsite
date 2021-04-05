02455aa1fdc17eb528b0d392b01d2910a2f28f3d
CactusKipic <clement.azan@gmail.com>
2021-04-06 00:43:24 +0200

    Premier jet à partir de zéro
    Spoiler: C'est pas ouf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialmoduleImport } from './materialmodule-import.module';
 
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    NavbarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialmoduleImport
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/materialmodule.spec.ts
@@ -0,0 +1,7 @@
+import { MaterialmoduleImport } from './materialmodule-import.module';
+
+describe('Materialmodule', () => {
+  it('should create an instance', () => {
+    expect(new MaterialmoduleImport()).toBeTruthy();
+  });
+});
